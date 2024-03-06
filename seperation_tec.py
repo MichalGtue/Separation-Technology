5a3b8559f4b2587f6441f4c7021cf50d0ca220b7
MichalGTue <m.j.gajda@student.tue.nl>
2024-03-06 16:58:50 +0100

    stripping done

--- seperation_tec.py
+++ seperation_tec.py
@@ -5,7 +5,16 @@ x_values = np.linspace(0,1, 1001)
 ## michal testttttttttttttttt
 
 x_feed = 0.05
-
+Aiso = 4.861
+Biso = 1357.427
+Ciso = -75.814
+Aw = 5.0768
+Bw = 1659.793
+Cw = -45.854
+Piso = 10**(Aiso - (Biso)/(Ciso + 81+273))
+Pw = 10**(Aw - (Bw)/(Cw + 81+273))
+alpha = Piso/Pw
+print(alpha)
 ## Stripping
 x_strip = np.linspace(0.013739,x_feed,100)
 #y_strip = 2.019*x_strip - 0.014
@@ -53,7 +62,7 @@ plt.plot(x_strip, y_strip, label = 'Stripping Line', color = 'green')
 #feed line/ q- line 
 
 
-plt.vlines(0.05, ymin=0, ymax=1, label = 'Feed - Line')
+plt.vlines(0.05, ymin=0, ymax=1, linestyles='dashed', label = 'Feed - Line')
 #plotting starting and ending point (if we want to add them)
 # plt.plot(0.05 , start_point, 'o') 
 
@@ -63,6 +72,8 @@ def func1(x):
     return (-5000 * x) / (4773 * x - 9730)
 def func2(x):
     return 0.93 * x + 0.04
+def func3(x):
+    return (185.96/92.1)*x - ((93.86/92.1)*0.014)
 initial_value = 0.6
 y_min_storage = 0.6
 
@@ -76,9 +87,15 @@ for _ in range(7):
     plt.hlines(new_y_value, xmax=x_min_value, xmin=func1(0.93 * x_min_value + 0.04))
     initial_value = 0.93 * x_min_value + 0.04
 
+for _ in range(8):
+    x_min_value = func1(initial_value)
+    plt.vlines(x_min_value, ymax=initial_value, ymin=func3(x_min_value))
+    new_y_value = func3(x_min_value)
+    if _ <7:
+        plt.hlines(new_y_value, xmax=x_min_value, xmin=func1((185.96/92.1)*x_min_value - ((93.86/92.1)*0.014)))
+    initial_value = (185.96/92.1)*x_min_value - ((93.86/92.1)*0.014)
 
-
-
+plt.vlines(0.014, ymin=0, ymax=1, linestyles='dashed', colors='teal')
 # Show the plot
 plt.minorticks_on()
 plt.grid(which = 'major', linewidth = 1)
